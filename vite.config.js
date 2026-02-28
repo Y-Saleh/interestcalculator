b61afa40ba7ec94072a1eefde9b0bd689b64abe8
yussuf <yussuf6704@gmail.com>
2026-02-28 02:15:44 +0000

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/interestcalculator/',
   plugins: [react()],
   resolve: {
     alias: {
